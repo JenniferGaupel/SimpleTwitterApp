1dd0c216bb9b36123d67139388c0ccfe0df48e5b
JenniferGaupel <jennifergaupel@gmail.com>
2019-11-12 19:06:04 -0500

    Backend user authentication implementing JsonWebToken WIP

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,44 +1,50 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :authorize_request, except: :create
+  before_action :find_user, except: %i[create index]
 
   # GET /users
   def index
     @users = User.all
-
-    render json: @users
+    render json: @users, status: :ok
   end
 
-  # GET /users/1
+  # GET /users/{username}
   def show
-    render json: @user
+    render json: @user, status: :ok
   end
 
   # POST /users
   def create
     @user = User.new(user_params)
-
     if @user.save
       render json: @user, status: :created, location: api_v1_user_url(@user)
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: { errors: @user.errors.full_messages },
+              status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /users/1
+  # PUT /users/{username}
   def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
+    unless @user.update(user_params)
+      render json: { errors: @user.errors.full_messages },
+             status: :unprocessable_entity
     end
   end
 
-  # DELETE /users/1
+  # DELETE /users/{username}
   def destroy
     @user.destroy
   end
 
   private
+
+    def find_user
+      @user = User.find_by_username!(params[:_username])
+      rescue ActiveRecord::RecordNotFound
+        render json: { errors: 'User not found' }, status: :not_found
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
@@ -46,6 +52,6 @@ class Api::V1::UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:username, :password, :created_at)
+      params.permit(:username, :password, :password_confirmation, :created_at)
     end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, :followings, :posts
+      resources :users, param: :_usernames
+      post '/auth/login', to: 'authentication#login'
+      get '/*a', to: 'application#not_found'
+      resources :followings
+      resources :posts
     end
   end
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
