14b6dc713942de680c72ae4c8b4dadb30249eebe
JenniferGaupel <jennifergaupel@gmail.com>
2019-11-15 15:54:32 -0500

    Created basic unit tests and added validation to post model

--- backend/test/models/post_test.rb
+++ backend/test/models/post_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save post without text" do
+    post = Post.new
+    assert_not post.save, "Post was saved without text"
+  end
 end
--- backend/test/models/user_test.rb
+++ backend/test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save user without username or password" do
+    user = User.new
+    assert_not user.save, "User was saved without username or password"
+  end
 end
