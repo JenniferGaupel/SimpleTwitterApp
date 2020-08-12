aae30fa6ba47cf63e2237c32836bd550d444402e
JenniferGaupel <jennifergaupel@gmail.com>
2020-08-12 15:44:06 -0400

    Added vote type to Votes

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_192105) do
+ActiveRecord::Schema.define(version: 2020_08_12_193723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_08_12_192105) do
   create_table "votes", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "vote_type"
   end
 
   add_foreign_key "followings", "users", column: "followed_id"
