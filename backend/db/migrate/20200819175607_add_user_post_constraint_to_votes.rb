188338d5fddaf179e469382024c6905f951e4c49
JenniferGaupel <jennifergaupel@gmail.com>
2020-08-19 14:04:40 -0400

    Add unique constraint to user & post on the votes table

---
+++ backend/db/migrate/20200819175607_add_user_post_constraint_to_votes.rb
@@ -0,0 +1,5 @@
+class AddUserPostConstraintToVotes < ActiveRecord::Migration[6.0]
+  def change
+    add_index :votes, [:user_id, :post_id], unique: true
+  end
+end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_181850) do
+ActiveRecord::Schema.define(version: 2020_08_19_175607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_08_13_181850) do
     t.bigint "post_id", null: false
     t.bigint "user_id", null: false
     t.index ["post_id"], name: "index_votes_on_post_id"
+    t.index ["user_id", "post_id"], name: "index_votes_on_user_id_and_post_id", unique: true
     t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
